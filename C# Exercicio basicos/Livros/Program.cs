7f75fb30b658a7731218b0a130b2af44021787e4
ManoUPdev <emanuelcgalindo@hotmail.com>
2025-04-21 19:09:18 -0300

    livros

--- C# Exercicio basicos/Livros/Program.cs
+++ C# Exercicio basicos/Livros/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+class Program
+{
+    static void Main(String[] args)
+    {
+        Livro meuLivro = new Livro();
+        meuLivro.Titulo = "O Senhor do Aneis";
+        meuLivro.Autor = "J.R.R. Tolkien";
+        meuLivro.NumeroPaginas = 1216;
+
+
+        meuLivro.ExibirDetalhes();
+    }
+}
