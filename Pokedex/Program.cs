6c248a9cd176a987ba93bb046e5b986d1333c9e3
ManoUPdev <emanuelcgalindo@hotmail.com>
2025-04-22 11:01:05 -0300

    Pokedex m2

--- Pokedex/Program.cs
+++ Pokedex/Program.cs
@@ -9,15 +9,15 @@ var app = builder.Build();
 // Lista de pokémons fictícios para simulação
 var pokemons = new List<Pokemon>
 {
-    new Pokemon { ID = 1, Nome = "Bulbasaur", Tipo = "Grama/Veneno"},
-    new Pokemon { ID = 2, Nome = "Ivysaur", Tipo = "Grama/Veneno"},
-    new Pokemon { ID = 3, Nome = "Venusaur", Tipo = "Grama/Veneno"},
-    new Pokemon { ID = 4, Nome = "Chamander", Tipo = "Fogo"},
-    new Pokemon { ID = 5, Nome = "Charmeleon", Tipo = "Fogo"},
-    new Pokemon { ID = 6, Nome = "Charizard", Tipo = "Fogo/Voador"},
-    new Pokemon { ID = 7, Nome = "Squirtle", Tipo = "Agua"},
-    new Pokemon { ID = 8, Nome = "Wartotle", Tipo = "Agua"},
-    new Pokemon { ID = 9, Nome = "Blastoise", Tipo = "Agua"}
+    new Pokemon { Id = 1, Nome = "Bulbasaur", Tipo = "Grama/Veneno"},
+    new Pokemon { Id = 2, Nome = "Ivysaur", Tipo = "Grama/Veneno"},
+    new Pokemon { Id = 3, Nome = "Venusaur", Tipo = "Grama/Veneno"},
+    new Pokemon { Id = 4, Nome = "Chamander", Tipo = "Fogo"},
+    new Pokemon { Id = 5, Nome = "Charmeleon", Tipo = "Fogo"},
+    new Pokemon { Id = 6, Nome = "Charizard", Tipo = "Fogo/Voador"},
+    new Pokemon { Id = 7, Nome = "Squirtle", Tipo = "Agua"},
+    new Pokemon { Id = 8, Nome = "Wartotle", Tipo = "Agua"},
+    new Pokemon { Id = 9, Nome = "Blastoise", Tipo = "Agua"}
 };
 
 // Endpoint para listar todos os pokémons
@@ -30,7 +30,7 @@ return Results.Ok(pokemons);
 app.MapGet("/pokemons/{id}", ( int id) =>
 
 {
-var pokemon = pokemons.FirstOrDefault(p => p.ID == id);
+var pokemon = pokemons.FirstOrDefault(p => p.Id == id);
 return pokemon is not null ? Results.Ok(pokemon) : Results.NotFound();
 });
 
