a888ebc1ba03036a5727a5df861726b36301c43d
ManoUpdev <emanuelcgalindo@hotmail.com>
2025-05-06 10:40:19 -0300

    reworking update

--- Pokedex/PokedexContext.cs
+++ Pokedex/PokedexContext.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 
     public class PokedexContext : DbContext
     {
-        public PokedexContext(DbContextOptions<PokedexContext> options) : base(options) { }
+        public PokedexContext(DbContextOptions<PokedexContext> options) 
+             : base(options) { }
 
         public DbSet<Pokemon> Pokemons { get; set; } = null!;
     }
--- Pokedex/Program.cs
+++ Pokedex/Program.cs
@@ -1,30 +1,31 @@
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<PokedexContext>(options =>
-options.UseSqlite("Data Source=pokedex.db"));
+builder.Services.AddDbContext<PokedexContext>(options = >
+   options.UseSqlite("Data Source=pokedex.db"));
+
+
 var app = builder.Build();
 
-
-// Lista de pokémons fictícios para simulação
-var pokemons = new List<Pokemon>
-{
-    new Pokemon { Id = 1, Nome = "Bulbasaur", Tipo = "Grama/Veneno"},
-    new Pokemon { Id = 2, Nome = "Ivysaur", Tipo = "Grama/Veneno"},
-    new Pokemon { Id = 3, Nome = "Venusaur", Tipo = "Grama/Veneno"},
-    new Pokemon { Id = 4, Nome = "Chamander", Tipo = "Fogo"},
-    new Pokemon { Id = 5, Nome = "Charmeleon", Tipo = "Fogo"},
-    new Pokemon { Id = 6, Nome = "Charizard", Tipo = "Fogo/Voador"},
-    new Pokemon { Id = 7, Nome = "Squirtle", Tipo = "Agua"},
-    new Pokemon { Id = 8, Nome = "Wartotle", Tipo = "Agua"},
-    new Pokemon { Id = 9, Nome = "Blastoise", Tipo = "Agua"}
-};
-
-// Endpoint para listar todos os pokémons
-
-});
+// Funcoes da Pokedex
 
 
 
-//Iniciar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.Run();
